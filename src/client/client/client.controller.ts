c0fc1e9a79a7d2f3b36321c321b381093ceea7da
Santo <santicardonas777@gmail.com>
2021-08-31 15:41:08 -0500

    Se modifico en el html docs

--- src/client/client/client.controller.ts
+++ src/client/client/client.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+import axios from 'axios';
 
 @Controller('client')
-export class ClientController {}
+export class ClientController {
+  //   @Get('axios')
+  //   public async getPets(@Res() response: Response) {
+  //     const data = await this.knex('test').select('*');
+  //     const requesPets: await axios.get;
+  //     const dataCategory = await axios.get(
+  //       'https://bsl1.herokuapp.com/pet/categories',
+  //     );
+  //     const category: IPetCategory = dataCategory.data;
+  //     const petConjuntion = pet.map((pets) => {
+  //       delete pets.id;
+  //       pets.category = category.categories.find(
+  //         (category) => category.id === pets.category,
+  //       ).name;
+  //       return pets;
+  //     });
+  //     return response.status(HttpStatus.OK).send({ petConjuntion });
+  //   }
+
+  @Get('axios')
+  public async getPets(@Res() response: Response) {
+    const datatPet = await axios.get('https://bsl1.herokuapp.com/pet');
+    const pet: IPet[] = datatPet.data;
+    const dataCategory = await axios.get(
+      'https://bsl1.herokuapp.com/pet/categories',
+    );
+    const category: IPetCategory = dataCategory.data;
+
+    const petConjuntion = pet.map((pets) => {
+      delete pets.id;
+      pets.category = category.categories.find(
+        (category) => category.id === pets.category,
+      ).name;
+      return pets;
+    });
+
+    return response.status(HttpStatus.OK).send({ petConjuntion });
+  }
+
+  @Get('axios')
+  public async getTask(@Res() response: Response) {
+    const requesPets = await axios.get(
+      'https://bsl1-proyect.herokuapp.com/pet',
+    );
+    const pets = requesPets.data.pets;
+    const requestCategories = await axios.get(
+      'https://bsl1-proyect.herokuapp.com/pet/categories',
+    );
+    const categorias = requestCategories.data.categorias;
+    const petsWithCatName = pets.map((pet) => {
+      delete pet.id;
+    });
+  }
+}
